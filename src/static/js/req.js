cd34ffa52a634979d7b0b6f638eb837642a49cee
psx980626 <375180728@qq.com>
2017-12-18 16:57:15 +0800

    排行榜修改

--- src/static/js/req.js
+++ src/static/js/req.js
@@ -7,8 +7,32 @@ function getRank() {
         $(".myimg").html('<img src=' + myimg + '>');
         $(".nickname-info").html(mynickname);
         $(".place").html(myrank);
+        console.log(data);
         console.log(data.data);
+        var rankdata = data.data;
 
+        for (var i = 1; i < 16; i++) {
+
+            var rankimgurl = rankdata[i].imgurl,
+                ranknickname = rankdata[i].nickname,
+                rank = rankdata[i].rank;
+            var placeItem = document.createElement("li"),
+                placeImg = document.createElement("div"),
+                placeNickname = document.createElement("div");
+            $(placeItem).attr("class", "placeItem");
+            $(placeImg).attr("class", "placeImg");
+            $(placeNickname).attr("class", "placeNickname");
+            $(".placeList").append(placeItem)
+            placeItem.append(placeImg);
+            placeItem.append(placeNickname);
+            var img = document.createElement("img"),
+                span = document.createElement("span");
+            $(span).attr("class", "nicknameword")
+            $(img).attr("src", rankimgurl);
+            placeNickname.append(span);
+            placeImg.append(img);
+            $(span).html(ranknickname);
+        }
     })
 }
 
